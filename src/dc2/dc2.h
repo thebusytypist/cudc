4ad82df0d7ef312e4339d2879c8274d6002219ac
thebusytypist <ounanding@gmail.com>
2015-11-10 23:42:34 -0800

    Comment the parameters of function DualContour2.

--- src/dc2/dc2.h
+++ src/dc2/dc2.h
@@ -6,6 +6,22 @@ enum FunctionType {
     cFunctionTypeCount
 };
 
+/**
+  \brief Dual contour a 2D function.
+
+  @param xs the starting x coordinates.
+  @param xt the ending x coordinates.
+  @param ys the starting y coordinates.
+  @param yt the ending y coordinates.
+
+  @param[out] p the generated mesh vertices stored as array of float2.
+  @param pcap the capacity of the vertices.
+  @param[out] pcnt the actual count of generated vertices.
+
+  @param[out] edges the generated edges stored as array int2.
+  @param ecap the capacity of the edges.
+  @param[out] ecnt the actual count of generated edges.
+*/
 bool DualContour2(
     FunctionType ft,
     float xs, float xt,
