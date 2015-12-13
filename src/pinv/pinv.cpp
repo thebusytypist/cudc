e25806d4243cafc465309b674f30f191987234f0
thebusytypist <ounanding@gmail.com>
2015-12-12 16:58:45 -0800

    Truncate singular values under 0.1.

--- src/pinv/pinv.cpp
+++ src/pinv/pinv.cpp
@@ -46,7 +46,7 @@ bool SVD2(
 bool PseudoInverse2(
     const float* M,
     float* pinv) {
-    const float EPS = 1e-7f;
+    const float EPS = 1e-1f;
 
     float U[4], S[2], VT[4];
     SVD2(M, U, S, VT);
